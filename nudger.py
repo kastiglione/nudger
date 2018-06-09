e66864601ec853b2883892dd2d8fdf465f2fcd76
Dave Lee <davelee.com@gmail.com>
2018-06-09 13:52:43 -0700

    Nudge last view when no view is provided

--- nudger.py
+++ nudger.py
@@ -9,21 +9,13 @@ def nudger(debugging, input, context, result, _):
     frame = context.frame
 
     args = input.split(" ", 2)
-    if len(args) != 3:
-        result.SetError("Usage: nudger x y view")
-        return
-
-    (x, y, viewExpression) = args
-
-    # Evaluate the view expression using the language of the current function,
-    # Swift or ObjC. The SBValue object return from EvaluateExpression() will
-    # expose the view's address, which can be inserted in the below ObjC.
-    view = frame.EvaluateExpression(viewExpression)
-
-    # A value always has an error object, the `fail` property indicates when an
-    # error actually occurred.
-    if view.error.fail:
-        result.SetError(view.error.description)
+    if len(args) == 3:
+        (x, y, viewExpression) = args
+    elif len(args) == 2:
+        (x, y) = args
+        viewExpression = None
+    else:
+        result.SetError("Usage: nudger x y [view]")
         return
 
     options = lldb.SBExpressionOptions()
@@ -33,12 +25,26 @@ def nudger(debugging, input, context, result, _):
     # without littering type casts throughout.
     frame.EvaluateExpression("@import UIKit", options)
 
-    viewExpression = "(UIView *){}".format(view.value)
+    if viewExpression:
+        # Evaluate the view expression using the language of the current function,
+        # Swift or ObjC. The SBValue object return from EvaluateExpression() will
+        # expose the view's address, which can be inserted in the below ObjC.
+        view = frame.EvaluateExpression(viewExpression)
+        if view.error.fail:
+            # A value always has an error object, the `fail` property indicates
+            # when an error actually occurred.
+            result.SetError(view.error.description)
+            return
+
+        # Initialize the $nudgedView variable. The $-prefix makes it persist
+        # across invocations of nudger.
+        initExpression = "UIView *$nudgedView = (UIView *){}".format(view.value)
+        frame.EvaluateExpression(initExpression, options)
+
     nudgeExpression = """
-        UIView *nudgedView = {viewExpression};
-        nudgedView.frame = CGRectOffset(nudgedView.frame, {x}, {y});
+        $nudgedView.frame = CGRectOffset($nudgedView.frame, {x}, {y});
         [CATransaction flush];
-    """.format(viewExpression=viewExpression, x=x, y=y)
+    """.format(x=x, y=y)
 
     error = frame.EvaluateExpression(nudgeExpression, options).error
     if error.fail and error.value != kNoResult:

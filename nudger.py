3c49f34ecc3df9b96386a52468a1aaa517d2427f
Dave Lee <davelee.com@gmail.com>
2018-06-09 13:40:45 -0700

    Support Swift input

--- nudger.py
+++ nudger.py
@@ -15,17 +15,32 @@ def nudger(debugging, input, context, result, _):
 
     (x, y, viewExpression) = args
 
-    # Import type info into lldb, otherwise lldb can't evaluate the ObjC
-    # without the unconventional use of type casts.
-    frame.EvaluateExpression("@import UIKit")
+    # Evaluate the view expression using the language of the current function,
+    # Swift or ObjC. The SBValue object return from EvaluateExpression() will
+    # expose the view's address, which can be inserted in the below ObjC.
+    view = frame.EvaluateExpression(viewExpression)
 
+    # A value always has an error object, the `fail` property indicates when an
+    # error actually occurred.
+    if view.error.fail:
+        result.SetError(view.error.description)
+        return
+
+    options = lldb.SBExpressionOptions()
+    options.SetLanguage(lldb.eLanguageTypeObjC)
+
+    # Import type info into lldb, otherwise lldb can't evaluate the ObjC below
+    # without littering type casts throughout.
+    frame.EvaluateExpression("@import UIKit", options)
+
+    viewExpression = "(UIView *){}".format(view.value)
     nudgeExpression = """
         UIView *nudgedView = {viewExpression};
         nudgedView.frame = CGRectOffset(nudgedView.frame, {x}, {y});
         [CATransaction flush];
     """.format(viewExpression=viewExpression, x=x, y=y)
 
-    error = frame.EvaluateExpression(nudgeExpression).error
+    error = frame.EvaluateExpression(nudgeExpression, options).error
     if error.fail and error.value != kNoResult:
         result.SetError(error.description)
         return
